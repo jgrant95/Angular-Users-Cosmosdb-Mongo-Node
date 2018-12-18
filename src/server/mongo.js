a894ce46216ae64fa42939369fad0e6c216cd8a4
Jonathan Grant <jonathangrant@etech.net>
2018-12-18 13:42:42 +0000

    attempt to make mongojs work

--- src/server/mongo.js
+++ src/server/mongo.js
@@ -6,12 +6,26 @@ const env = require('./env/environment')
 
 const mongoUri = `mongodb://${env.dbName}:${env.key}@${env.dbName}.documents.azure.com:${env.cosmosPort}/?ssl=true`
 
+const mongo = {
+    uri: mongoUri,
+    opt: {
+        // dbName: env.dbName,
+        // useNewUrlParser: true
+    }
+  };
+  
+
+
 function connect() {
-    return mongoose.connect(mongoUri, { useNewUrlParser: true }, function(err, database) {
-        if(err) return console.error(err);
-      
-        db = database;
-    })
+    return mongoose.connect(mongo.uri, mongo.opt).then(
+        () => {
+            console.log('database connection successful')
+            return mongoose.connection
+        },
+        (err) => {
+            console.log('database connection failed', err)
+        }
+    )
 }
 
 module.exports = {
