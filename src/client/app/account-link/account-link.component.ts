ec6c524e4edd96d8ecb6aad2415a9f11dbe36ade
jgrant95 <jongrant95@gmail.com>
2019-01-04 15:09:28 +0000

    Sort out routing, add account-link component too

---
+++ src/client/app/account-link/account-link.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'atr-account-link',
+  templateUrl: './account-link.component.html',
+  styleUrls: ['./account-link.component.scss']
+})
+export class AccountLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+}
--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { AccountLinkComponent } from './account-link/account-link.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AccountLinkComponent },
+  { path: 'users', component: UsersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
